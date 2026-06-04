641a4c29b1b6f554b8836a5fe1424cb8e9b83a9b
diegomarln <josue.marin.lopezd@gmail.com>
2026-06-04 02:15:17 -0600

    chore(backend): add EF Core DbContext foundation

---
+++ src/Inventory.Api/Data/InventoryDbContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Inventory.Api.Data;
+
+public class InventoryDbContext : DbContext
+{
+    public InventoryDbContext(DbContextOptions<InventoryDbContext> options)
+        : base(options)
+    {
+    }
+}
