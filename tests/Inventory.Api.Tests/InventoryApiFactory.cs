904d17fedca262af188e89813f1d011f1a8de5e4
diegomarln <josue.marin.lopezd@gmail.com>
2026-06-04 11:18:35 -0600

    test(backend): add smoke tests and CI workflow

---
+++ tests/Inventory.Api.Tests/InventoryApiFactory.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+
+namespace Inventory.Api.Tests;
+
+public class InventoryApiFactory : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.UseEnvironment("Development");
+        builder.ConfigureAppConfiguration((_, config) =>
+        {
+            config.AddInMemoryCollection(new Dictionary<string, string?>
+            {
+                ["ConnectionStrings:DefaultConnection"] =
+                    "Server=test;Database=InventoryDbTest;User Id=sa;Password=unused;Encrypt=False;TrustServerCertificate=True;"
+            });
+        });
+    }
+}
